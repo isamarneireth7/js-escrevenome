acf5449df16457baed874cb10878f056fa7a5241
isamarneireth7 <isamar.ortiz@escola.pr.gov.br>
2024-08-05 09:32:41 -0300

    adicionando aquivos js e scc

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(600, 600);
+ }
+
+
+
+function draw() {
+  background("black");
+stroke(blue)
+  fill(red)
+if(mouseIspressed);{
+  rect(mousex, mousey, 20,35);
+}
+}
+
+
